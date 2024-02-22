7e867e3d7004bb0d5b8a8026d7d5296ae6877476
fibarrola <francisco.ibarrola@outlook.com>
2024-02-23 08:00:40 +1100

    moved imports to __init__.py for cleaner imports of packages

--- tryit.py
+++ tryit.py
@@ -1,5 +1,5 @@
-from image_diversity.clip_metrcis import ClipMetrics
-from image_diversity.inception_metrics import InceptionMetrics
+from image_diversity import ClipMetrics
+from image_diversity import InceptionMetrics
 
 div_clip = ClipMetrics()
 div_inception = InceptionMetrics()
