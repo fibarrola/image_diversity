99e6bb89d864d5d8945c5c1403f1bf6bfc2d1ac2
fibarrola <francisco.ibarrola@outlook.com>
2024-02-23 14:52:56 +1100

    reduced number of images

--- tests/test_clip_metrics.py
+++ tests/test_clip_metrics.py
@@ -7,13 +7,13 @@ class TestClipMetrics:
     
     def test_tcd_negis(self):
         """TCE should not work if the number of eigenvalues is smaller than the number of images"""
-        clip_metrics.n_eigs = 50
+        clip_metrics.n_eigs = 5
         with pytest.raises(AssertionError):
             clip_metrics.tce("tests/image_set_1")
 
     def test_tcd_output(self):
         """TCE should produce a positive value"""
-        clip_metrics.n_eigs = 5
+        clip_metrics.n_eigs = 2
         assert clip_metrics.tce("tests/image_set_1") > 0, "TCE value should be positive"
 
     def test_tcd_subset(self):
@@ -23,13 +23,13 @@ class TestClipMetrics:
 
     def test_fcd_negis(self):
         """FCD should not work if the number of eigenvalues is smaller than the number of images"""
-        clip_metrics.n_eigs = 50
+        clip_metrics.n_eigs = 5
         with pytest.raises(AssertionError):
             clip_metrics.fcd("tests/image_set_1", "tests/image_set_2")
 
     def test_fcd_output(self):
         """FCD should not work if the number of eigenvalues is smaller than the number of images"""
-        clip_metrics.n_eigs = 5
+        clip_metrics.n_eigs = 2
         assert clip_metrics.fcd("tests/image_set_1", "tests/image_set_2") > 0, "FCD value should be positive"
 
     def test_fcd_diff_subsets(self):
@@ -40,5 +40,5 @@ class TestClipMetrics:
     
     def test_fcd_subsets(self):
         """FCD should produce a positive value"""
-        clip_metrics.n_eigs = 1
+        clip_metrics.n_eigs = 2
         assert clip_metrics.fcd("tests/image_set_1", "tests/image_set_2", ["A_10.png", "A_30.png", "A_50.png"], ["V_10.png", "V_30.png", "V_50.png"]) > 0
