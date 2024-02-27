f15184fd04da0cc88ac5d5b4e7addb2f16a1f5a0
fibarrola <francisco.ibarrola@outlook.com>
2024-02-27 11:12:14 +1100

    linting

--- src/image_diversity/clip_metrcis.py
+++ src/image_diversity/clip_metrcis.py
@@ -1,6 +1,5 @@
 from .clip import clip
 import torch
-import os
 import warnings
 from PIL import Image
 from scipy import linalg
--- src/image_diversity/utils.py
+++ src/image_diversity/utils.py
@@ -3,7 +3,12 @@ import os
 
 VALID_IMG_FORMATS = ["jpeg", "png", "gif", "bmp", "tiff"]
 
+
 def get_img_names(path):
     filenames = os.listdir(path)
-    img_names = [fname for fname in filenames if imghdr.what(os.path.join(path, fname)) in VALID_IMG_FORMATS]
+    img_names = [
+        fname
+        for fname in filenames
+        if imghdr.what(os.path.join(path, fname)) in VALID_IMG_FORMATS
+    ]
     return img_names
